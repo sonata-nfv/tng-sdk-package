8e69bb43db7f4633681cf0d9aa8feaf88fdfe8e0
nandoooo <fernando.diaz@atos.net>
2020-06-30 10:47:14 +0000

    ignore E741 flake8

--- pipeline/checkstyle/check.sh
+++ pipeline/checkstyle/check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 echo "checking style ..."
-docker run -i --rm registry.sonata-nfv.eu:5000/tng-sdk-package flake8 --exclude .eggs .
+docker run -i --rm registry.sonata-nfv.eu:5000/tng-sdk-package flake8 --ignore=E741 --exclude .eggs .
 echo "done."
 # always exit with 0 (ugly code style is not an error :))
 #exit 0
